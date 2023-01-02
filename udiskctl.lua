8dd15d76f53a0bcb59dae8276ccb1daa3ffb26c7
flintforge <phil.estival@free.fr>
2023-01-02 12:29:14 +0100

    udiskctl still in dev

--- udiskctl.lua
+++ udiskctl.lua
@@ -1,12 +1,10 @@
--------------------------------------------------
--- Usb disks control for Awesome Window Manager
+---------------------------------------------------
+-- Usb disks controls for Awesome Window Manager
 
--- @author Phil Estival pe [@t] 7d.nz
+-- @author Phil Estival ©2021
 -- https://github.com/flintforge/awesome-wm-widgets
 
--- handles powering drives
--- requires udisksctl
-----------------------------------------------------
+---------------------------------------------------
 
 local awful = require("awful")
 local wibox = require("wibox")
@@ -21,44 +19,69 @@ local WIDGET_DIR = WIDGETS_DIR .. ''
 local ICONS_DIR = WIDGETS_DIR ..'/icons/'
 local ICON = 'usb-tree.svg'
 
-local DRIVES = "for drives in $(lsblk -do name,tran | awk '$2==\"usb\"{print $1}'); do  lsblk /dev/$drives -o name,mountpoint | awk 'NR!=1' ; done"
----> sdb1/WDntfs
-local poweroff="udisksctl power-off -b "
+local status_cmd = "lsblk -Pmino name,label,mountpoint,size,state,vendor,model "
+local usb_drives_cmd = 'for drive in $(lsblk -do name,tran | awk \'$2=="usb"{print $1}\'); do '..
+   status_cmd..' /dev/$drive ; done'
+
+-- using series of spawn_easy_async is too hairy without a queue
+--local usb_drives_cmd = 'lsblk -do name,tran | awk \'$2=="usb"{print $1}\''
+--local usb_partitions_cmd = "lsblk -Pmino name,label,mountpoint,size,state,vendor,model "
+
+local poweroff_cmd = "udisksctl power-off -b "
+local mount_cmd    = "udisksctl mount -b "
+local unmount_cmd  = "udisksctl unmount -b "
+local open_cmd     = "xdg-open "
+
+
 local MARGINS = 4
-local SPACINGS = 4
+local VSPACE = 4
 local HEIGHT = 15
 
--- local DISKS = [[ sh -c "lsblk -o name,mountpoint | /bin/grep media |tr -d '  ' | cut -d '/' -f1,4 | sed -e 's/├─//'" ]]
--- local USBDEV = "lsblk -do name,tran | awk '$2==\"usb\"{print $1}'"
--- local function get_drives (dev, callback)
---    spawn.easy_async_with_shell(
---       "lsblk /dev/" .. dev .." -o name,mountpoint | awk 'NR!=1'",
---       callback)
--- end
-
-
---- Utility function to show warning messages
-local function show_warning(message)
+--- show warning messages
+local function notify(message)
    naughty.notify{
-      preset = naughty.config.presets.critical,
+      preset = naughty.config.presets.low,
       title = 'Error',
       text = message}
 end
-
-local function mount(part)
-   show_warning(part)
-   spawn.easy_async("mount /dev/" .. part,
-		    function(stdout,stderr)
-		       show_warning(stderr)
-		    end
+local function spawncmd(cmd)
+   spawn.easy_async(
+      cmd,
+      function(stdout,stderr)
+	 notify(stderr)
+      end
    )
 end
+
+local function mount(part)
+   spanwcmd( mount_cmd.."/dev/"..part.name )
+end
+
 local function umount(part)
-   spawn.easy_async_with_shell( "umount /dev/" .. part,
-				function(stdout,stderr)
-				   show_warning(stderr)
-                                end
-   )				  
+   spawncmd( unmount_cmd.."/dev/"..part.name )
+end
+local function poweroff(drive)
+   notify("ok powering off")
+   --spawn(power_off_cmd)
+end
+
+local function open(path)
+   spawncmd( open_cmd..path )
+end
+
+local function confirm(text,next)
+   awful.prompt.run {
+      prompt       = text.."(type 'y' to confirm)? ",
+      textbox      = awful.screen.focused().mypromptbox.widget,
+      exe_callback = function (t)
+         if string.lower(t) == "y" then
+            next()
+         end
+      end,
+      -- completion_callback = function (t, p, n)
+      --    return awful.completion.generic(t, p, n, {"no", "NO", "yes", "YES"})
+      -- end
+   }
 end
 
 function in_table ( t, e )
@@ -67,7 +90,6 @@ function in_table ( t, e )
    end
    return false
 end
-
 
 local function ellipsize(text, length)
    return (text:len() > length and length > 0)
@@ -87,10 +109,8 @@ local wibox_popup = wibox {
    border_color = beautiful.bg_focus,
    max_widget_size = 500,
    height = 100,
-   width = 250,
-   bg = "#666"
-   --beautiful.bg_normal
-   -- opacity = 1,
+   width = 450,
+   bg = "#555"
 }
 
 local udiskswidget = wibox.widget {
@@ -109,94 +129,223 @@ local udiskswidget = wibox.widget {
 }
 
 
-local function makebox(label,checked, click)
-   return wibox.widget {
-      {{
-	    { {
-		  id = 'checkbox',
-		  checked       = checked, --mtpt and true or false,--actives[screen],
-		  color         = beautiful.bg_normal,
-		  paddings      = 2,
-		  shape         = gears.shape.square,
-		  check_color   = beautiful.fg_normal,
-		  forced_width  = 15,
-		  forced_height = HEIGHT,
-		  border_color  = beautiful.border_focus,
-		  border_width  = 1,
-		  widget        = wibox.widget.checkbox
-	      },
-	       valign = 'center',
-	       layout = wibox.container.place,
-	    },
-	    spacing = SPACING,
-	    {  {
-		  id = 'name',
-		  --markup = '<b>' .. package['name'] .. '</b>',
-		  markup = label, -- mtpt and disk .. "/" .. mtpt or disk,
-		  widget = wibox.widget.textbox
-	       },
-	       halign = 'left',
-	       layout = wibox.container.place
-	    },	   
-	    spacing = SPACING,
-	    layout = wibox.layout.fixed.horizontal
-       },
-	 margins = MARGINS,
-	 layout = wibox.container.margin
-      },
-      id = 'row',
-      bg = beautiful.bg_normal,
-      widget = wibox.container.background,
-      click = click      
+
+-- local function makebox(label,checked, click)
+--    return wibox.widget {
+--       {{
+-- 	    { {
+-- 		  id = 'checkbox',
+-- 		  checked       = checked,
+-- 		  color         = beautiful.bg_normal,
+-- 		  paddings      = 2,
+-- 		  shape         = gears.shape.square,
+-- 		  check_color   = "#0A0", -- beautiful.fg_normal,
+-- 		  forced_width  = 15,
+-- 		  forced_height = HEIGHT,
+-- 		  border_color  = beautiful.border_focus,
+-- 		  border_width  = 1,
+-- 		  widget        = wibox.widget.checkbox
+-- 	      },
+-- 	       valign = 'center',
+-- 	       layout = wibox.container.place,
+-- 	    },
+-- 	    spacing = SPACING,
+-- 	    {  {
+-- 		  id = 'name',
+-- 		  --markup = '<b>' .. package['name'] .. '</b>',
+-- 		  markup = label, -- mtpt and disk .. "/" .. mtpt or disk,
+-- 		  widget = wibox.widget.textbox
+-- 	       },
+-- 	       halign = 'left',
+-- 	       layout = wibox.container.place
+-- 	    },
+-- 	    spacing = SPACING,
+-- 	    layout = wibox.layout.fixed.horizontal
+--        },
+-- 	 margins = MARGINS,
+-- 	 layout = wibox.container.margin
+--       },
+--       id = 'row',
+--       bg = beautiful.bg_normal,
+--       widget = wibox.container.background,
+--       click = click
+--    }
+-- end
+
+
+local function make_checkbox(partition, active, phy, drive)
+   local shape
+   local width
+   if phy then
+      width=15
+      shape = gears.shape.rounded_bar
+   else
+      witdh=25
+      shape = gears.shape.square
+   end
+
+   local cb = wibox.widget {
+      widget        = wibox.widget.checkbox,
+      checked       = active,
+      id = 'checkbox',
+      color         = beautiful.bg_normal,
+      paddings      = 2,
+      shape         = shape,
+      check_color   = "#0A0", -- beautiful.fg_normal,
+      forced_width  = width,
+      forced_height = HEIGHT,
+      border_color  = beautiful.border_focus,
+      border_width  = 1,
+      click = function(self, checked)
+
+	 if phy and active
+	 then
+	    local poff = function()
+	       poweroff(d)
+	       self:set_checked(not self.checked)
+	    end
+	    confirm("unmount paritions and power off drive ? ", poff)
+
+	    --mount(d)
+	 else
+	    if drive
+	    then
+	       if active
+	       then
+		  print("unmount"..partition.mountpoint)
+		  umount(partition)
+		  self:set_checked(not self.checked)
+		  --worker() TODO : refresh state
+	       else
+		  print("mount "..partition.name)
+		  mount(partition)
+		  self:set_checked(not self.checked)
+	       end
+	    end
+	 end
+      end
    }
+
+   cb:connect_signal(
+      "button::press", function(c, _, _, button)
+	 if button == 1 then c:click() end end)
+   return cb
+end
+
+
+local function make_path(partition)
+   local path = wibox.widget {
+      widget = wibox.widget.textbox,
+      text = partition.mountpoint,
+      forced_width = 10,
+      click = function(self)
+	 if (not(partition.mountpoint=="")) then
+	    open(partition.mountpoint)
+	 end
+      end
+   }
+
+   path:connect_signal(
+      "button::press", function(c, _, _, button)
+	 if button == 1 then c:click() end end)
+   return path
+end
+
+local function makerow(partition, active, phy, drive)
+   return wibox.widget
+      {
+	 make_checkbox(partition,active,phy, drive),
+	 {
+	    text = partition.name,
+	    forced_width = 10,
+	    widget = wibox.widget.textbox
+	 },
+	 {
+	    text = partition.label,
+	    forced_width = 10,
+	    widget = wibox.widget.textbox
+	 },
+	 make_path(partition),
+	 layout = wibox.layout.ratio.horizontal
+      }
+end
+
+function values(t)
+   local i = 0
+   return function() i = i + 1; return t[i] end
+end
+
+
+function lpad(str, len, char)
+    if char == nil then char = ' ' end
+    return str .. string.rep(char, len - #str)
 end
 
 
 local function worker(user_args)
 
    local args = user_args or {}
-   local icon = args.icon or ICONS_DIR .. ICON
-   -- todo: make it an other if there's no disk to mount or poweroff (black)
-
-   udiskswidget:set_icon(icon)
+   local icon = args.icon or ICONS_DIR..ICON
 
    local min_widgets = 5
    local carousel = false
-   
+
+   udiskswidget:set_icon(icon)
+
+   spawn.easy_async_with_shell(
+      usb_drives_cmd,
+      function(stdout, stderr)
+	 -- todo: make it an other if there's no disk to mount or poweroff (black)
+	 drives=stdout
+	 notify(drives)
+	 --udiskswidget:set_icon(icon_cold)
+	 --udiskswidget:set_icon(icon_error)
+      end
+   )
+
+   local n=0
+
+   local function partitions(dr)
+      local drives= {}
+      local match = 'NAME="(.*)" '
+	 ..'LABEL="(.*)" MOUNTPOINT="(.*)" '
+	 ..'SIZE="(.*)" STATE="(.*)" '
+	 ..'VENDOR="(.*)" MODEL="(.*)"'
+
+      n=0
+      print(dr)
+      for li in dr:gmatch("[^\r\n]+") do
+	 --print(li)
+	 local name,label,mountpoint,size,state,vendor,model = li:match(match)
+	 --print(name,label,mountpoint)
+	 drives[n] = {name=name, label=label, mountpoint=mountpoint,
+		      size=size, state=state, vendor=vendor, model=model}
+	 --print(drives[i].name)
+	 n = n+1
+      end
+      return drives
+   end
+
+
    local function rebuild_widget(ddrives, errors, _, _)
-      
+
+      -- notify(ddrives)
       if errors ~= '' then
-	 show_warning(errors)
+	 notify(errors)
 	 return
       end
-      
-      local rows = wibox.layout.fixed.vertical()
-      local i=0
-      
-      -- {disk = d, parts = {}}     
-      local parts={}
-      local drives = {}
-      local disk
-      for line in ddrives:gmatch("[^\r\n]+") do
-	 --local disk, mtpt = line:match("(.*)/(.*)")1
-	 print(line)
-	 local part,mtpt = line:match("├─(.*)%s(.*)") 
-	 if part == nil
-	 then --attemp 2 (lua has no multistring match)
-	    part,mtpt = line:match("└─(.*)%s(.*)") 
-	 end
-	 
-	 if part == nil
-	 then
-	    drives[line] = {}
-	    disk = line
-	 else 
-	    print(part, mtpt)
-	    table.insert(drives[disk], {part=part, mtpt=mtpt})
-	 end
-      end
 
-      print("[",ddrives,"]")
+--       confirmQuitmenu = awful.menu(
+-- 	 { items = {
+-- 	      { "Cancel", function() do end end },
+      -- 	      { "Quit", function() awesome.quit() end }}}
+      -- ..
+--     awful.key({ modkey, "Shift"   }, "q", function () confirmQuitmenu:show() end,
+--               {description = "Confirm Awesome wm exit", group = "awesome"}),
+
+      local rows = wibox.layout.fixed.vertical()
+
+
       if ddrives == '' then
 	 local row = wibox.widget {
 	    {{
@@ -208,86 +357,71 @@ local function worker(user_args)
 	       layout = wibox.container.place
 	 }}
 	 rows:add(row)
-      end
-      
-      for d,parts in pairs(drives) do
-	 
-	 local row = makebox(
-	    d, true, -- poweredoff disk would be undetected
-	    function(self, checked)
-	       local a = self:get_children_by_id('checkbox')[1]
-	       -- turn_screen(screen, not a.checked)
-	       a:set_checked(not a.checked)
-	       --n = a.checked and -1 or 1
-	       if parts
-	       then 
-		  if not a.checked
-		  then
-		     poweroff(d)
-		  else
-		     mount(d)
-		  end
-	       end
-	       --n_actives = n_actives + n
-	       --show_warning( "".. n_actives )
-	 end)
-	 
-	 row:connect_signal("mouse::enter", function(c)
-			       c:set_bg(beautiful.bg_focus)
-	 end)
-	 row:connect_signal("mouse::leave", function(c)
-			       c:set_bg(beautiful.bg_normal)
-	 end)		  
-	 row:connect_signal("button::press", function(c, _, _, button)
-			       if button == 1 then c:click() end
-	 end)
-	 rows:add(row)
-	 i = i+1
 
-	 --show_warning(d)
-	 for k,part in ipairs(parts) do
-	    print(" ".. part.part .. " : " .. (part.mtpt or '') )
-	    local row = makebox(
-	       "   " .. part.part .. " ", part.mtpt,
-	       function(self, checked)
-		  local a = self:get_children_by_id('checkbox')[1]
-		  -- turn_screen(screen, not a.checked)
-		  a:set_checked(not a.checked)
-		  --n = a.checked and -1 or 1
-		  if not a.checked
-		  then
-		     umount(part.part)
-		  else
-		     mount(part.part)
-		  end
-		  --n_actives = n_actives + n
-		  --show_warning( "".. n_actives )
-	    end)
-	    
-	    row:connect_signal("mouse::enter", function(c)
-				  c:set_bg(beautiful.bg_focus)
-	    end)
-	    row:connect_signal("mouse::leave", function(c)
-				  c:set_bg(beautiful.bg_normal)
-	    end)		  
-	    row:connect_signal("button::press", function(c, _, _, button)
-				  if button == 1 then c:click() end
-	    end)
-	    rows:add(row)
-	    i = i+1
+      else
+	 local drives = partitions(ddrives)
+	 for i=0,n-1 do
+	    local parts = drives[i]
+	    -- print(parts.name)
+	    -- print(parts.state)
+	    -- print(parts.mountpoint)
+	    -- print("---")
+    	    -- print(not(parts.mountpoint==""))
+	    -- print(not(parts.vendor==""))
+	    -- print("----")
+
+	    local active = false
+	    local phy = false
+	    local drive = false
+	    if (not(parts.vendor=="")) then -- a physical drive
+	       phy = true
+	       active = parts.state=="running"
+	       label = lpad(parts.name, 5)..'\t'.. parts.label..'\t'
+		  ..parts.vendor.." ".. parts.model.. parts.size
+	    else
+	       drive = true
+	       active = not(parts.mountpoint=="")
+	       label = lpad(parts.name, 5)..' '.. parts.label..'\t'..parts.mountpoint
+	    end
+
+
+	       -- id = 'row',
+	       -- bg = beautiful.bg_normal,
+	       -- halign='left',
+	       -- widget = wibox.container.background,
+
+	    local row=makerow(parts,active,phy,drive)
+	    row:ajust_ratio(2, 0.1, 0.2, 0.7)
+
+	    local line = wibox.widget(
+	    {
+	       row,
+	       id = 'row',
+	       bg = beautiful.bg_normal,
+	       halign='left',
+	       widget = wibox.container.background,
+	    })
+	    line:connect_signal(
+	       "mouse::enter", function(c) c:set_bg(beautiful.bg_focus) end)
+	    line:connect_signal(
+	       "mouse::leave", function(c) c:set_bg(beautiful.bg_normal) end)
+	    -- row:connect_signal(
+	    --    "button::press", function(c, _, _, button)
+	    -- 	  if button == 1 then c:click() end end)
+	    rows:add(line)
 	 end
-	 
       end
 
-      
       wibox_popup:setup {
 	 rows,
+	 margins = 30,
 	 layout = wibox.layout.fixed.vertical,
+	 padding = 10,
+	 border_width = 10,
       }
-      
-      wibox_popup.height = (  HEIGHT + MARGINS + SPACINGS) * (i)
-      
+      wibox_popup.height = (  HEIGHT + MARGINS + VSPACE +1 ) * (n-1)
    end
+
 
    udiskswidget:buttons(
       awful.util.table.join(
@@ -296,8 +430,8 @@ local function worker(user_args)
 		  wibox_popup.visible = not wibox_popup.visible
 	       else
 		  spawn.easy_async_with_shell(
-		     DRIVES,
-		     function(stdout, stderr) 
+		     usb_drives_cmd,
+		     function(stdout, stderr)
 			rebuild_widget(stdout or ' ', stderr or ' ')
 			wibox_popup.visible = true
 			awful.placement.top(
