50a5f9e5cfb08c57fae47888a9d8e0063cbd219a
flintforge <phil.estival@free.fr>
2023-03-10 12:15:53 +0100

    direct set method & custom default temp

--- sct.lua
+++ sct.lua
@@ -12,73 +12,84 @@
 
 -----------------------------------------------------
 
-local awful	= require("awful")
-local spawn	= require("awful.spawn")
-local wibox	= require("wibox")
+local awful = require("awful")
+local spawn = require("awful.spawn")
+local wibox = require("wibox")
 
-local function worker(user_args)
+local function worker(...)
 
-  local temperature = 6500 -- kelvins
+	 local arg = {n=select('#',...),...}
+	 local default_temp = arg[1]
+   local temperature = default_temp -- kelvins
 
-  local text = wibox.widget {
+   local text = wibox.widget {
       id = "txt",
       font   = "Inconsolata Medium 13",
       widget = wibox.widget.textbox
-  }
+   }
 
-  local sct = wibox.widget {
-     forced_height = 32,
-     forced_width = 44,
-     bg = "#000",
-     paddings = 0,
-     widget = wibox.widget.textbox,
-  }
+   local sct = wibox.widget {
+      forced_height = 32,
+      forced_width = 44,
+      bg = "#000",
+      paddings = 0,
+      widget = wibox.widget.textbox,
+   }
 
-  local function get_temperature()
-     sct.markup = string.format(" <b>%.1fK</b>",temperature/1000)
-  end
+   local function get_temperature()
+      sct.markup = string.format(" <b>%.1fK</b>",temperature/1000)
+   end
 
-  get_temperature()
+   get_temperature()
 
-  local update_graphic = function(widget, stdout, _, _, _)
-     widget.colors = { colors.B }
-  end
+   local update_graphic = function(widget, stdout, _, _, _)
+      widget.colors = { colors.B }
+   end
 
-  local function updateTemperature(tpr)
-     temperature = temperature + tpr
-     if(temperature<1000) then temperature = 1000 end
-     if(temperature>10000) then temperature = 10000 end
-     awful.spawn("sct " .. temperature,false)
-     get_temperature()
-  end
+   local function updateTemperature(tpr)
+      temperature = temperature + tpr
+      if(temperature<1000) then temperature = 1000 end
+      if(temperature>10000) then temperature = 10000 end
+      awful.spawn("sct " .. temperature,false)
+      get_temperature()
+   end
 
-  local function defaultTemperature()
-     temperature = 6400
-     awful.spawn("sct", false)
-     get_temperature()
-  end
+   local function defaultTemperature()
+      temperature = default_temp
+      awful.spawn("sct", false)
+      get_temperature()
+   end
 
-  sct:connect_signal(
-     "button::press",
-     function(_, _, _, button)
-	if (button == 3 or button==5) then
-	   updateTemperature( -500 )
-	elseif (button == 1 or button==4) then
-	   updateTemperature(  500 )
-	elseif (button == 2) then
-	   defaultTemperature()
-	end
-  end)
+   local function setTemperature(t)
+      temperature = t
+      awful.spawn("sct", false)
+      get_temperature()
+   end
 
-  local Sct = {
-     widget = sct,
-     update = updateTemperature,
-     defaultTemperature = defaultTemperature
-  }
-  return Sct
+   sct:connect_signal(
+      "button::press",
+      function(_, _, _, button)
+         if (button == 3 or button==5) then
+            updateTemperature( -500 )
+         elseif (button == 1 or button==4) then
+            updateTemperature(  500 )
+         elseif (button == 2) then
+            defaultTemperature()
+         end
+   end)
+
+   local Sct = {
+      widget = sct,
+      update = updateTemperature,
+      set = setTemperature,
+      defaultTemperature = defaultTemperature
+   }
+   return Sct
 end
 
 local sct_widget = {}
-return setmetatable( sct_widget, {
-      __call = function(_, ...) return worker(...) end
-})
+return setmetatable(
+	 sct_widget, {
+			__call = function(_, ...) return worker(...) end
+							 }
+)
